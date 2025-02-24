0d45a8a8795c00b07650234e47a7abbaf2615e08
costacoder <acosta.coder@gmail.comcleargit config --global user.email acosta.coder@gmail.com>
2025-02-23 22:19:35 -0300

    atualizando app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <medidasObra/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import medidasObra from './components/medidas-obra.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    medidasObra,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
